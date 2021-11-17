7ffb4b0c59854767486227d10e0a4b28dadf2563
romain-morvant <romainmorvantdev@gmail.com>
2021-11-17 14:31:06 +0100

    Ajout de commentaires pour chaque fonction

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d', { alpha: true });
 const rangeSlider = document.getElementById('slider__range');
-// let rangeValue = rangeSlider.value;
 
-
-// Redimensionnement
+// Redimensionnement de l'élément canvas à chaque rechargement de la page
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 
@@ -24,6 +22,10 @@ function makeIntArray(rows, cols) {
     return intArray;
 }
 
+// Cette fonction reçoit trois paramètres qui sont
+// 1- rows = le nombre de lignes que l'on souhaite afficher
+// 2- cols = le nombre de colonnes que l'on souhaite afficher
+// 3- count = le nombre de grilles (rows*cols) que l'on souhaite
 function genMultipleArrays(rows, cols, count) {
     let array = [];
     for (let i = 0; i < count; i++) {
@@ -42,9 +44,9 @@ function drawGrid(grid, cellSize = 40) {
         for (let row = 0; row < grid[col].length; row++) {
             let x = row * (cellSize + gap);
             let y = col * (cellSize + gap);
-            let color = grid[col][row];
-            ctx.font = '14px sans-serif';
+            let color = grid[col][row]; // Je stocke un entier générer plus haut dans cette variable et l'utiliserai ensuite pour le choix de couleur de la cellule.
             ctx.fillStyle = 'hsl(' + color + ',60%,60%)';
+            ctx.font = '14px sans-serif';
             ctx.fillRect(x, y, cellSize, cellSize);
             ctx.strokeText(`${color}`, x + (cellSize / 4), y + (cellSize / 2));
             ctx.fill();
@@ -52,9 +54,12 @@ function drawGrid(grid, cellSize = 40) {
     }
 }
 
-let extGrid = genMultipleArrays(10, 10, 40);
-// console.log(extGrid);
-drawGrid(extGrid[9], 100);
+// Ici je stocke le résultat de la fonction genMultipleArrays dans la variable extGrid, qui devient ensuite la grille donnée en paramètre de la fonction drawGrid.
+let extGrid = genMultipleArrays(10, 12, 10);
+console.log(extGrid);
+
+// Ici j'appelle la fonction drawGrid, le premier paramètre est une grille et le second paramètre est la valeur en pixel du coté de nos cellules dans la grille.
+drawGrid(extGrid[9], 40);
 
 rangeSlider.addEventListener('change', (e) => {
     drawGrid(extGrid[+e.target.value]);
